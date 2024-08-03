c3f759f1992ea874ca327b55816ac5a64eb46461
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2024-08-03 12:00:27 +0200

    passage à xcode

---
+++ metro.config.js
@@ -0,0 +1,34 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const defaultConfig = getDefaultConfig(__dirname);
+
+const customConfig = {
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+        assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+        sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+    },
+};
+
+// Fusionner la configuration par défaut avec les personnalisations
+module.exports = {
+    ...defaultConfig,
+    transformer: {
+        ...defaultConfig.transformer,
+        ...customConfig.transformer,
+    },
+    resolver: {
+        ...defaultConfig.resolver,
+        ...customConfig.resolver,
+    },
+};
